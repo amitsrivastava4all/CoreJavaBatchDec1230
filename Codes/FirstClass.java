67b773c9f908a7ab1d77fc4b1f5a0b2b6139afe7
amit4alljava <amit4alljava@gmail.com>
2016-12-27 14:41:46 +0530

    Codes

---
+++ Codes/FirstClass.java
@@ -0,0 +1,34 @@
+strictfp class SecondClass{
+public static void main(String []arr){
+    //int x = 100;
+    //String y = "amit";
+    /*
+    Data Types
+    1. Primitive (Value Type) - C , C++
+    byte - 1 Byte
+    short -2 Byte
+    int - 4 Byte
+    long- 8 Byte
+    float - 4 Byte (IEEE-754)
+    double - 8 Byte
+    boolean - 1 Byte , 2 Byte , 4 Byte (Depends on O/s to O/s)
+    char -2 Byte
+    
+    2. Reference 
+    BigInteger
+    BigDecimal
+    String
+    */
+    // 1 Byte - 8 Bit
+     // 7 Bit = Number 1 Bit +-
+    // -2 ^ 7 to 2 ^ 7 -1
+    // -128 to 127
+    byte age = 21;
+    byte r = (byte)130;
+    long t = 10l;
+    float pp = 10.29f;
+    
+    System.out.println("Hello Java "+age+" "+r);
+    System.out.println(10.20);
+}
+}
