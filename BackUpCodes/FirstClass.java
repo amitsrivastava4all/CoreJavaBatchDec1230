199d03e798708f2dbe446496e918a135b461c8c0
amit4alljava <amit4alljava@gmail.com>
2017-01-17 16:37:56 +0530

    Codes

---
+++ BackUpCodes/FirstClass.java
@@ -0,0 +1,23 @@
+class SecondClass{
+// Package = Folder
+	// main is execution point of a program
+	// public (OutSide the Package)- main has a public access so java command can access ur main
+	// static - It has only one copy in the memory, it is load when class is loaded
+	// void  - not return type
+	// String - It is a predefine class in Java, and String is basically a character array
+	// String is a collection of characters 
+	// args it is just a variable name and it can be anything
+	// args is always an array type
+	// String args[] - this is command line argument
+	public static void main(String arr[]){
+		System.out.println("Hello Java ");
+		/*
+		System - is a predefine class
+		out is predefine object of PrintStream Class
+		println is a predefine method of PrintStream class
+		println - printline - always print in a new line
+		print - it print in a same line
+		System - in , out , err
+		*/
+	}
+}
